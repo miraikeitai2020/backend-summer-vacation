a76652602070cbd2ad9eb1e64b8c7cf690f9c200
SaKu2110 <b1018106@fun.ac.jp>
2020-08-16 12:52:58 +0900

    Add skeleton code

---
+++ pkg/server/server.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+	// import gin library
+	"github.com/gin-gonic/gin"
+
+	// import sample API packages
+	"github.com/miraikeitai2020/backend-summer-vacation/pkg/server/controller"
+)
+
+func initializeController() controller.Controller {
+	return controller.Controller{}
+}
+
+func setupRooter(ctrl controller.Controller) *gin.Engine {
+	router := gin.Default()
+	// API Handlers List
+	router.GET("/", ctrl.HelloWorld)
+	router.POST("/sayhello", ctrl.SayHello)
+	// 以下， 課題の進行状況に応じてコメントアウトを外す
+	// router.GET("/task1", ctrl.Task1)
+	// router.POST("/task2", ctrl.Task2)
+	// router.POST("/task4", ctrl.SignUp)
+	// router.POST("/task4", ctrl.SignIn)
+	return router
+}
+
+func main() {
+	ctrl := initializeController()
+	router := setupRooter(ctrl)
+	err := router.Run(":8080")
+	if err != nil {
+		panic("server Painc")
+	}
+}
