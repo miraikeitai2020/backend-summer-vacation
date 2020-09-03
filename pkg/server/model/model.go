4739aaaadc4ee08a05fc4c2da862855f532abcd3
SaKu2110 <b1018106@fun.ac.jp>
2020-09-03 19:59:29 +0900

    Add timestamp struct

--- pkg/server/model/model.go
+++ pkg/server/model/model.go
@@ -3,3 +3,14 @@ package model
 type User struct {
 	Name	string `json:"name"`
 }
+
+// TASK1 RESPONSE BODY
+type TimeStamp struct {
+	TimeStamp	string	`json:"timestamp"`
+	Detail		Detail	`json:"datail"`
+}
+
+type Detail struct {
+	Date	string	`json:"date"`
+	Time	string	`json:"time"`
+}
